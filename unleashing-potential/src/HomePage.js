d1c4d9fbb81f70d06c48f8a4efc78c66295c2574
Lithemba Nkqayi <99335731+Lithemba352@users.noreply.github.com>
2024-08-20 20:27:16 +0000

    Added more pages

--- unleashing-potential/src/HomePage.js
+++ unleashing-potential/src/HomePage.js
@@ -1,22 +1,30 @@
 // src/HomePage.js
 import React from 'react';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div className="home-page">
       <nav className="navbar">
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#courses">Courses</a></li>
-          <li><a href="#blog">Blog</a></li>
-          <li><a href="#achievements">Achievements</a></li>
-          <li><a href="#partners">Partners</a></li>
-          <li><a href="#about-us">About Us</a></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/achievements">Achievements</Link></li>
+          <li><Link to="/partners">Partners</Link></li>
+          <li><Link to="/about-us">About Us</Link></li>
         </ul>
       </nav>
+
+      <div className="quote">
+        <h1>Connecting Aspiring IT talent with innovative ICT Leaders for a Brighter Future</h1>
+      </div>
+
       <div className="buttons">
-        <button className="explore-btn">Explore Courses</button>
+        <Link to="/courses">
+          <button className="explore-btn">Explore Courses</button>
+        </Link>
         <button className="view-blogs-btn">View Blogs</button>
       </div>
     </div>
---
+++ unleashing-potential/src/Partners.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Partners.css';
+
+function Partners() {
+  return (
+    <div className="page">
+      <h1>Partners</h1>
+    </div>
+  );
+}
+
+export default Partners;
