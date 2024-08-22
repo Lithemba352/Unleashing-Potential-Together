0e795347f0d52aa567850c4ab0c76147ed676327
Lithemba Nkqayi <99335731+Lithemba352@users.noreply.github.com>
2024-08-22 22:23:40 +0000

    Updated Achievements

--- unleashing-potential/src/Achievements.js
+++ unleashing-potential/src/Achievements.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Achievements.css';
+import Badge from './images/Badge.jpg'
 
 function Achievements() {
   return (
-    <div className="page">
-      <h1>Achievements</h1>
+    <div className="achievements-page">
+      <div className="achievement-header">
+        <h1>Our Achievements</h1>
+      </div>
+      <img src={Badge} alt="Achievement Bage" className="achievement-badge" />
+      <div className="achievement-content">
+        <h2>Empowering Over 1,000 IT Professionals:</h2>
+        <p>
+          Through our dedicated skill development programs, we have supported more than 
+          1,000 aspiring IT professionals in enhancing their expertise and advancing their 
+          careers. Our training initiatives are designed to equip individuals with the knowledge 
+          and skills needed to thrive in the rapidly evolving tech landscape.
+        </p>
+      </div>
     </div>
   );
 }
